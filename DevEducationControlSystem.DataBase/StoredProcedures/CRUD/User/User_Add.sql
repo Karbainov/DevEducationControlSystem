fac542cfc0ac8ee3f3f304f97b539acc0b9e5708
Ivan <vancir@gmail.com>
2021-01-13 03:06:57 +0300

    User CRUD procedures created

---
+++ DevEducationControlSystem.DataBase/StoredProcedures/CRUD/User/User_Add.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE User_Add
+	@Id int,
+	@StatusID int,
+	@FirstName nvarchar(30),
+	@LastName nvarchar(30),
+	@BirthDate date,
+	@Login nvarchar(15), 
+	@Password nvarchar(8),
+	@email nvarchar(30),
+	@Phone nvarchar(12),
+	@ContractNumber nvarchar(15),
+	@ProfileImage nvarchar(1000)
+AS
+INSERT [User] VALUES (
+	@Id, 
+	@StatusID, 
+	@FirstName, 
+	@LastName, 
+	@BirthDate,
+	@Login, 
+	@Password, 
+	@email, 
+	@Phone, 
+	@ContractNumber,
+	@ProfileImage
+	
+)
---
+++ DevEducationControlSystem.DataBase/StoredProcedures/CRUD/User/User_Update.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE User_Update
+	@Id int,
+	@StatusID int,
+	@FirstName nvarchar(30),
+	@LastName nvarchar(30),
+	@BirthDate date,
+	@Login nvarchar(15), 
+	@Password nvarchar(8),
+	@email nvarchar(30),
+	@Phone nvarchar(12),
+	@ContractNumber nvarchar(15),
+	@ProfileImage nvarchar(1000)
+AS  
+	UPDATE [User]
+	SET
+	StatusID = @StatusID,
+	FirstName = @FirstName,
+	LastName = @LastName,
+	BirthDate = @BirthDate,
+	Login = @Login, 
+	Password = @Password,
+	email = @email,
+	Phone = @Phone,
+	ContractNumber = @ContractNumber,
+	ProfileImage = @ProfileImage
+WHERE  
+	Id=@Id
