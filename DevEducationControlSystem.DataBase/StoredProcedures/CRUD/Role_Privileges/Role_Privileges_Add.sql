1facea58083294541933b9f2ce54b7dc4b1eafbc
NickWhiteman <romanov_nikita@hotmail.com>
2021-01-14 22:53:00 +0300

    fix err

--- DevEducationControlSystem.DataBase/StoredProcedures/CRUD/Role_Privileges/Role_Privileges_Add.sql
+++ DevEducationControlSystem.DataBase/StoredProcedures/CRUD/Role_Privileges/Role_Privileges_Add.sql
@@ -1,5 +1,6 @@
 CREATE PROCEDURE [dbo].[Role_Privileges_Add]
+	@Id int,
 	@RoleID int,
 	@PrivilegesID int
 AS
-INSERT [dbo].[Role_Privileges] VALUES (@RoleID, @PrivilegesID)
+INSERT [dbo].[Role_Privileges] VALUES (@Id ,@RoleID, @PrivilegesID)
