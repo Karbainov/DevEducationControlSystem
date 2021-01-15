ee559d3a1e94080c25c456efaf44be7b2334fc2d
El_makso <El_makso@mail.ru>
2021-01-15 13:18:49 +0300

    Create new DBP and import from server

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Tables/Group_Material.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Group_Material] (
+    [Id]         INT IDENTITY (1, 1) NOT NULL,
+    [GroupId]    INT NOT NULL,
+    [MaterialId] INT NOT NULL,
+    CONSTRAINT [PK_Group_Material] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Group_Material_Group] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Group] ([Id]),
+    CONSTRAINT [FK_Group_Material_Material] FOREIGN KEY ([MaterialId]) REFERENCES [dbo].[Material] ([Id])
+);
+
---
+++ DevEducationControlSystem.DataBaseProject/dbo/Tables/Theme.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Theme] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (255) NOT NULL,
+    CONSTRAINT [PK_Theme] PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([Name] ASC)
+);
+
