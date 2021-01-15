cae39bad690d199b9a29eb6432ab2dbe59a553c3
Oleg Lyamzin <o.s.like@yandex.ru>
2021-01-15 17:25:06 +0300

    Create CRUD procedures for Group_Material

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/CRUD/Group_Material/Group_Material_Add.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE Group_Material_Add
+	@GroupID int,
+	@MaterialID int
+AS
+INSERT [Group_Material] VALUES (@GroupID,
+	@MaterialID)
+	go
---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/CRUD/Group_Material/Group_Material_Update.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE Group_Material_Update  
+	@Id int,	
+	@GroupID int,
+	@MaterialID int
+AS  
+	UPDATE [Group_Material] 
+	SET
+	[GroupID] = @GroupID,
+	[MaterialID] = @MaterialID
+WHERE  
+	Id=@Id
