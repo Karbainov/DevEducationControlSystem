2496521c5a0613c91c169c340dcb67263010ebc4
Oleg Lyamzin <o.s.like@yandex.ru>
2021-01-15 17:14:13 +0300

    Create CRUD procedures for Group

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/CRUD/Group/Group_Add.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE Group_Add
+	@Id int,
+	@StatusID int,
+	@CourseID int,
+	@CityID int,
+	@Name nvarchar(100),
+	@StartDate date
+AS
+INSERT [Group] VALUES (@Id,
+	@StatusID,
+	@CourseID,
+	@CityID,
+	@Name,
+	@StartDate)
---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/CRUD/Group/Group_Update.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE Group_Update  
+	@Id int,
+	@StatusID int,
+	@CourseID int,
+	@CityID int,
+	@Name nvarchar(30),
+	@StartDate date
+AS  
+	UPDATE [Group] 
+	SET
+	StatusID = @StatusID,
+	CourseID = @CourseID,
+    CityID = @CityID,
+	[Name] = @Name,
+	StartDate = @StartDate
+WHERE  
+	Id=@Id
