d37e20613af75c99ce2b73c0f07e0f9a786e18ed
Oleg Lyamzin <o.s.like@yandex.ru>
2021-01-15 17:17:57 +0300

    Create CRUD procedures for City

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/CRUD/City/City_Add.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE City_Add	
+	@Name nvarchar(100)
+AS
+INSERT [City] VALUES (@Name)
+	
---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/CRUD/City/City_Update.sql
@@ -0,0 +1,9 @@
+﻿	CREATE PROCEDURE City_Update  
+	@Id int,	
+	@Name nvarchar(100)
+AS  
+	UPDATE [Group] 
+	SET
+	[Name] = @Name
+WHERE  
+	Id=@Id
