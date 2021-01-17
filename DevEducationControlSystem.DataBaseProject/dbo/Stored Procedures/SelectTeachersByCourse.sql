758b7ea404966387a10500841ed2268c0d3b0687
zeta0ne <space1signs@gmail.com>
2021-01-18 02:30:34 +0300

    SelectTeachersByCourse stored procedure added

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectTeachersByCourse.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[SelectTeachersByCourse]
+	@CourseId int
+as
+select *
+	from Course as C
+	inner join [Group] as G on C.Id=G.CourseId
+	inner join User_Group as UG on G.Id=UG.GroupId
+	inner join User_Role as UR on UG.UserId=UR.UserId
+	where G.CourseId = @CourseId
+	and UR.RoleId = '1'
