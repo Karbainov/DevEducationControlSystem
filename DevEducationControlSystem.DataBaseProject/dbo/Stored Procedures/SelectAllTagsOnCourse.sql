831f502e721c819df0e00451c2953531f372fbc6
zeta0ne <space1signs@gmail.com>
2021-01-18 04:29:57 +0300

    SelectAllTagsOnCourse created INPROGRESS

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectAllTagsOnCourse.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[SelectAllTagsOnCourse]
+as
+select * 
+	from Homework_Course as HC
+	inner join Homework_Tag as HT on HC.HomeworkId=HT.HomeworkId
+	inner join Course_Material as CM on HC.CourseId=CM.CourseId
+	right outer join Material_Tag as MT on HT.TagId=MT.TagId
