35d6914ff4434ff1317f814e9e03aa8291ab0941
Dmitriy Filimonov <banodannon@gmail.com>
2021-01-18 03:26:21 +0300

    GetAllGroupMaterialsId stored proc created
    Процедура получения Id всех материалов группы по id студента.

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/GetAllGroupMaterialsId.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE GetAllGroupMaterialsId
+	@UserId int
+AS
+BEGIN
+	DECLARE @GroupId int;
+
+	SET @GroupId = (SELECT GroupId FROM User_Group WHERE UserId=@UserId);
+
+	SELECT Course_Material.MaterialId FROM Course_Material
+		WHERE [Course_Material].[CourseId] = (SELECT [Group].[CourseId] FROM [Group] WHERE Id = @GroupId)
+	UNION
+	SELECT Group_Material.MaterialId FROM Group_Material WHERE Group_Material.GroupId = @GroupId;
+END
+GO
