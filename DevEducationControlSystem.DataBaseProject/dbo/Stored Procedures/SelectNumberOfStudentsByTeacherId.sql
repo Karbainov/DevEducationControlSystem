54215104d6acf148ff4d5c9f7f0182cc024a1af4
Oleg Lyamzin <o.s.like@yandex.ru>
2021-01-18 21:42:56 +0300

    Fix procedure

--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectNumberOfStudentsByTeacherId.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectNumberOfStudentsByTeacherId.sql
@@ -7,5 +7,5 @@ inner join [Group] as G on UG.GroupId = G.Id
 inner join [User_Role] as UR on U.Id = UR.UserId
 inner join (select G.Id as GroupId, G.Name as GroupName from [User] as U 
 	inner join [User_Group] as UG on U.Id = UG.UserId 
-	inner join [Group] as G on UG.GroupId = G.Id where U.Id = 33) as GROUPS on GROUPS.GroupId = G.Id
+	inner join [Group] as G on UG.GroupId = G.Id where U.Id = @UserId) as GROUPS on GROUPS.GroupId = G.Id
 where UR.RoleId = 2
