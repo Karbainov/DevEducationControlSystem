5f9a830e89364f84630c202d9cf3d559cd0f5a7b
first <pkpbi01@mail.ru>
2021-01-19 23:17:57 +0300

    Lesson to C#
    LessonDTO Dane

---
+++ DevEducationControlSystem.DBL/DTO/Base/LessonDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.DBL.DTO.Base
+{
+    public class LessonDTO
+    {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public string LessonDate { get; set; }
+        public string Comments { get; set; }
+
+        public LessonDTO()
+        {
+
+        }
+
+        public LessonDTO(
+            int id,
+            int groupId,
+            string name,
+            string lessonDate,
+            string comments)
+        {
+            Id = id;
+            GroupId = groupId;
+            Name = name;
+            LessonDate = lessonDate;
+            Comments = comments;
+        }
+    }
+}
