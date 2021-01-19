d8d56b4aea521bace27f845e3c559052001da7cf
julia <iulechkasirotkina@gmail.com>
2021-01-19 17:15:52 +0300

    Create Procedure

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectPaymentInfo.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[SelectPaymentInfo]
+	 @Login nvarchar(15)
+
+AS
+BEGIN
+
+   SELECT [Payment].Period, [Payment].IsPaid, [Payment].Sum, [Payment].PayDate FROM Payment
+   WHERE UserId =(SELECT Id From [User] WHERE [User].Login IN (@Login));
+
+END
