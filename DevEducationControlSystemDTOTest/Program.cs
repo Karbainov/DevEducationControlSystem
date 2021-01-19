51f05b82f5ba2adf249ec4666f51ccf285f5afa0
An <anchka.gor@yandex.ru>
2021-01-20 01:06:58 +0300

    consoletest create

---
+++ DevEducationControlSystemDTOTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevEducationControlSystemDTOTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
