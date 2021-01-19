267724dc871e814eef0c52f049fde1c7d23c43ae
julia <iulechkasirotkina@gmail.com>
2021-01-19 15:57:59 +0300

    Create Procedure SelectUserInfoByLogin

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectUserInfoByLogin.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[SelectUserInfoByLogin]
+	  @Login nvarchar(15)
+
+AS
+BEGIN
+   SELECT [User].Id,[User].FirstName, [User].LastName, [User].BirthDate, [User].Email, [User].Phone, [User].ProfileImage, [Role].Name  
+   FROM [User]
+ 
+
+   JOIN User_Role ON [User_Role].UserId = [User].Id
+   JOIN [Role] ON [User_Role].RoleId = [Role].Id
+     WHERE [User].Login IN (@Login)
+END
