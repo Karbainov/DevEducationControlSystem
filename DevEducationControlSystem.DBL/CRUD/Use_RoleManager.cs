8a43149e03eceee1734928d14e68b8927beb2de7
An <anchka.gor@yandex.ru>
2021-01-19 19:23:23 +0300

    Exception add

--- DevEducationControlSystem.DBL/CRUD/Use_RoleManager.cs
+++ DevEducationControlSystem.DBL/CRUD/Use_RoleManager.cs
@@ -18,20 +18,64 @@ namespace DevEducationControlSystem.DBL.CRUD
         public List<User_RoleDTO> Select()
         {
             var User_RoleDTOs = new List<User_RoleDTO>();
-            //Тут как-то заполняется
 
+            try
+            {
+                connection.Open();
+            }
+            catch
+            {
+                connection.Close();
+                throw new Exception("DataBase connection failed");
+            }
 
+            string sqlExpression = "EXEC User_Role_Select";
+            SqlCommand command = new SqlCommand(sqlExpression, connection);
+
+            try
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read()) // построчно считываем данные
+                        {
+                            var User_RoleDTO = new User_RoleDTO();
+
+                            User_RoleDTO.Id = (int)reader["Id"];
+                            User_RoleDTO.UserId = (int)reader["UserId"];
+                            User_RoleDTO.RoleId = (int)reader["RoleId"];
+
+                            User_RoleDTOs.Add(User_RoleDTO);
+                        }
+
+                    }
+
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
             return User_RoleDTOs;
-        }
 
+        }
         public User_RoleDTO SelectById(int id)
         {
             var User_RoleDTO = new User_RoleDTO();
             //Тут как-то заполняется
 
-            connection.Open();
+            try
+            {
+                connection.Open();
+            }
+            catch
+            {
+                connection.Close();
+                throw new Exception("Connection ne open");
+            }
 
-            string sqlExpression = "EXEC Course_Theme_SelectById " + id;
+            string sqlExpression = "EXEC User_Role_SelectById " + id;
             SqlCommand command = new SqlCommand(sqlExpression, connection);
             //command.CommandType = System.Data.CommandType.StoredProcedure;
 
@@ -44,18 +88,15 @@ namespace DevEducationControlSystem.DBL.CRUD
                 User_RoleDTO.UserId = (int)reader["UserId"];
                 User_RoleDTO.RoleId = (int)reader["RoleId"];
             }
-            catch
-            {
-
-            }
             finally
             {
                 reader.Close();
                 connection.Close();
             }
-            
+
 
             return User_RoleDTO;
         }
+        
     }
 }
