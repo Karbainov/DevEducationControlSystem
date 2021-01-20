d1b0a16594556d6e43a7edcf3a04d6d1faa13267
NickWhiteman <romanov_nikita@hotmail.com>
2021-01-20 17:55:36 +0300

    Add DTO => Description
    PrivilegesDTO
    Role_PrivilegesDTO

---
+++ DevEducationControlSystem.DBL/DTO/Base/Role_PrivilegesDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.DBL.DTO.Base
+{
+    class Role_PrivilegesDTO
+    {
+        public int Id { get; set; }
+
+        public int RoleId { get; set; }
+
+        public int PrivilegesId { get; set; }
+
+        public Role_PrivilegesDTO(Role_PrivilegesDTO dto)
+        {
+            this.Id = dto.Id;
+            this.RoleId = dto.RoleId;
+            this.PrivilegesId = dto.PrivilegesId;
+        }
+        
+    }
+}
