f4f6f1b3efcc6e15479657bb1a928b467abfb056
iabrmv <vancir@gmail.com>
2021-01-21 22:01:24 +0300

    Material DTO class created

---
+++ DevEducationControlSystem.DBL/DTO/Base/MaterialDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.DBL.DTO.Base
+{
+    public class MaterialDTO
+    {
+        public int Id;
+        public int UserId;
+        public int ResourceId;
+        public string Name;
+        public string Message;
+        public bool isDeleted;
+
+
+    }
+}
