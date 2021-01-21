4cc7b93ceb76950875b32391a4f36efb4ff12f40
Dmitriy Filimonov <banodannon@gmail.com>
2021-01-21 18:11:35 +0300

    SAVE BUTTON!

--- DevEducationControlSystem.DBL/TagsFromAllMaterials.cs
+++ DevEducationControlSystem.DBL/TagsFromAllMaterials.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using DevEducationControlSystem.DBL.DTO;
 using System.Text;
 
 namespace DevEducationControlSystem.DBL
 {
-    class SearchByTagsFromAllMaterials
+    class TagsFromAllMaterials
     {
+        List<TagsFromAllMaterialsDTO> materialsByTag;
+
+        public TagsFromAllMaterials()
+        {
+            string connectionString = @"Data Source=80.78.240.16; Initial Catalog=DevEdControl.Test;User Id=devEd; Password=qqq!11";
+            string expr = "[selectById]";
+            var value = new { Id = 1 };
+        }
+
+        public TagsFromAllMaterialsDTO Search(int userId, string tagName)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                materialsByTag = connection.Query<TagsFromAllMaterialsDTO>(expr, value, commandType: CommandType.StoredProcedure).AsList<DuckTypeDTO>();
+                materialsByTag.ForEach(r => Console.WriteLine(r.DuckType + " " + r.ID));
+            }
+            return materialsByTag;
+        }
     }
 }
