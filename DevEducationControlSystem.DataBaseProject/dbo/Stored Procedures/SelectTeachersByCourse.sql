e2602b6991b3b86b0fc8b6ea47d66e34c8a4f9ac
zeta0ne <space1signs@gmail.com>
2021-01-21 21:40:47 +0300

    small update

--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectTeachersByCourse.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectTeachersByCourse.sql
@@ -1,10 +1,12 @@
 ﻿CREATE PROCEDURE [dbo].[SelectTeachersByCourse]
 	@CourseId int
 as
-select *
-	from [Group] as G 
+select C.Id as CourseId, C.[Name] as [Name], G.CityId as CityId, CT.[Name] as CityName
+	from Course as C
+	inner join [Group] as G on C.Id=G.CourseId
+	inner join City as CT on G.CityId=CT.Id
 	inner join User_Group as UG on G.Id=UG.GroupId
 	inner join User_Role as UR on UG.UserId=UR.UserId
-	where G.CourseId=@CourseId
+	where G.CourseId = @CourseId
 	and UR.RoleId = '1'
 	go
