82783a32b1b38951bdffdb611bff4ff7aab704bc
Dmitriy Filimonov <banodannon@gmail.com>
2021-01-21 22:03:52 +0300

    made GetAllGroupMaterialsByUserId reusable

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/GetAllGroupMaterialsByUserId.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE GetAllGroupMaterialsByUserId
+	@UserId int
+AS
+
+BEGIN
+	
+	DECLARE @GroupId int;
+	SET @GroupId = (SELECT GroupId FROM User_Group WHERE UserId=17);
+
+	SELECT ResourceId, Links, Images FROM
+	(
+	SELECT Course_Material.MaterialId FROM Course_Material
+		WHERE [Course_Material].[CourseId] = (SELECT [Group].[CourseId] FROM [Group] WHERE Id=@GroupId)
+	UNION
+	SELECT Group_Material.MaterialId FROM Group_Material WHERE Group_Material.GroupId=@GroupId
+	) AS AllGroupMaterials
+	JOIN
+	[Material] ON AllGroupMaterials.MaterialId=Material.Id
+	JOIN
+	[Resource] On [Resource].Id=Material.ResourceId
+
+END
