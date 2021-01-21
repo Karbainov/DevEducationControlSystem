ac314e25925b4e4eb1a79db17daa2722b7334936
julia <iulechkasirotkina@gmail.com>
2021-01-21 19:52:14 +0300

    Add join in procedure

--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectUserInfoByLogin.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectUserInfoByLogin.sql
@@ -3,11 +3,17 @@
 
 AS
 BEGIN
-   SELECT [User].Id,[User].FirstName, [User].LastName, [User].BirthDate, [User].Email, [User].Phone, [User].ProfileImage, [Role].Name  
+   SELECT [User].Id,[User].FirstName, [User].LastName, [User].BirthDate, [User].Email, [User].Phone, [User].ProfileImage,
+   [Role].Name, [Group].Name, [Course].Name, [City].Name  
    FROM [User]
  
 
    JOIN User_Role ON [User_Role].UserId = [User].Id
    JOIN [Role] ON [User_Role].RoleId = [Role].Id
+   JOIN [User_Group] ON [User_Group].UserId = [User].Id
+   JOIN [Group] ON [Group].Id = [User_Group].GroupId
+   JOIN [Course] ON [Course].Id = [Group].CourseId
+   JOIN [City] ON [City].Id = [Group].CityId
+
      WHERE [User].Login IN (@Login)
 END
