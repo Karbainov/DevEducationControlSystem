e1f3ff16078e26904f508740dc35696de946b16f
Frendor <frendor.dm@gmail.com>
2021-01-21 23:03:47 +0300

    Fix Create SelectStudentsByGroupId

--- DevEducationControlSystem.DataBase/StoredProcedures/CRUD/Course_Homework/dbo.SelectStudentsByGroupId.sql
+++ DevEducationControlSystem.DataBase/StoredProcedures/CRUD/Course_Homework/dbo.SelectStudentsByGroupId.sql
@@ -7,6 +7,6 @@ join User_Group ug on u.id = ug.UserId
 join [Group] g on ug.GroupID = g.ID 
 join User_Role ur on u.Id = ur.UserId
 join Role r on ur.RoleId = r.Id
-where (GroupID = @GroupID) and (r.Name = 'Студент')
+where (GroupID = @GroupID) and (r.id = 2)
 
- 
+ exec SelectStudentsByGroupId 1
