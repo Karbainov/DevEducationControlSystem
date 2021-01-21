479f4447f0044ed4010d84377cbd2e7f198ea21b
Даниил <evkakiy.olen@gmail.com>
2021-01-21 21:34:33 +0300

    addStudentTeachersAndTutorsFromTheGroup

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/StudentTeachersAndTutorsFromTheGroup.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[StudentTeachersAndTutorsFromTheGroup]
+	@GroupId int
+AS
+	select UR.UserId, NAR.Name, [User].FirstName, [User].LastName, UG.GroupId, G.Name
+  from [User]
+  inner join [User_Role] as UR on [UR].UserId = [User].Id
+  inner join [Role] as NAR on [UR].RoleId = [NAR].Id
+  inner join [User_Group] as UG on [User].Id = [UG].UserId
+  inner join [Group] as G on [UG].GroupId = G.Id
+  where [G].Id = @GroupId
