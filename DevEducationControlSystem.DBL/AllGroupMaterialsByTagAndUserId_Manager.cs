c958e13cf7521575524e081594f42f22c0953fcd
Dmitriy Filimonov <banodannon@gmail.com>
2021-01-21 22:43:17 +0300

    Searching by tag classes naming fixed

--- DevEducationControlSystem.DBL/AllGroupMaterialsByTagAndUserId_Manager.cs
+++ DevEducationControlSystem.DBL/AllGroupMaterialsByTagAndUserId_Manager.cs
@@ -8,26 +8,26 @@ using System.Text;
 
 namespace DevEducationControlSystem.DBL
 {
-    public class TagsFromAllMaterials
+    public class AllGroupMaterialsByTagAndUserId_Manager
     {
         
         string connectionString;
         string expr;
 
-        public TagsFromAllMaterials()
+        public AllGroupMaterialsByTagAndUserId_Manager()
         {
             connectionString = @"Data Source=80.78.240.16; Initial Catalog=DevEdControl.Test;User Id=devEd; Password=qqq!11";
-            expr = "[SearchByTagsFromAllMaterials]";
+            expr = "[GetAllGroupMaterialsByTagAndUserId]";
             
         }
 
-        public List<TagsFromAllMaterialsDTO> Search(int userId, string tagName)
+        public List<AllGroupMaterialsByTagAndUserIdDTO> Get(int userId, string tagName)
         {
-            List<TagsFromAllMaterialsDTO> materialsByTag = new List<TagsFromAllMaterialsDTO>();
-            var value = new { GroupId = userId, TagName = tagName };
+            List<AllGroupMaterialsByTagAndUserIdDTO> materialsByTag = new List<AllGroupMaterialsByTagAndUserIdDTO>();
+            var value = new { UserId = userId, TagName = tagName };
             using (var connection = new SqlConnection(connectionString))
             {
-                materialsByTag = connection.Query<TagsFromAllMaterialsDTO>(expr, value, commandType: CommandType.StoredProcedure).AsList<TagsFromAllMaterialsDTO>();
+                materialsByTag = connection.Query<AllGroupMaterialsByTagAndUserIdDTO>(expr, value, commandType: CommandType.StoredProcedure).AsList<AllGroupMaterialsByTagAndUserIdDTO>();
                 //materialsByTag.ForEach(r => Console.WriteLine(r.DuckType + " " + r.ID));
             }
             return materialsByTag;
