c2ceb04c02be021b6c393a133ceac7bfee335dfb
zeta0ne <space1signs@gmail.com>
2021-01-22 00:03:28 +0300

    small fix

--- DevEducationControlSystem.DBL/CRUD/MaterialManager.cs
+++ DevEducationControlSystem.DBL/CRUD/MaterialManager.cs
@@ -66,7 +66,7 @@ namespace DevEducationControlSystem.DBL.CRUD
                 throw new Exception("Failed to connect");
             }
 
-            string sqlExpression = "Material_Select";
+            string sqlExpression = "Material_SelectById";
             SqlCommand command = new SqlCommand(sqlExpression, connection);
             command.CommandType = System.Data.CommandType.StoredProcedure;
             SqlParameter idParam = new SqlParameter("@Id", Id);
