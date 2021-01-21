d78bc90da543b23b37baa0c54ea5f09c594d2c67
Даниил <evkakiy.olen@gmail.com>
2021-01-21 22:19:11 +0300

    Thme&hemeTagDTOs

---
+++ DevEducationControlSystem.DBL/DTO/Base/ThemeDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.DBL.DTO.Base
+{
+    class ThemeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ThemeDTO()
+        {
+
+        }
+
+        public ThemeDTO(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+
+}
