319028e2c11ccf2308101f034098f7683c639b8f
Dmitriy Filimonov <banodannon@gmail.com>
2021-01-22 10:50:45 +0300

    reuse done

--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/GetAllGroupMaterialsByTagAndUserId.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/GetAllGroupMaterialsByTagAndUserId.sql
@@ -5,27 +5,19 @@
 
 AS
 BEGIN
---переиспользование GetAllGroupMaterialByUserId, как сделать? SELECT * FROM(EXEC имя_процедуры) не работает
+
 DECLARE @GroupId int;
 	SET @GroupId = (SELECT GroupId FROM User_Group WHERE UserId=@UserId);
---конец переиспользования
+
+DECLARE @var table (ResourceId int, MaterialId int, Links nvarchar(1000), Images nvarchar(1000))
+INSERT @var EXEC GetAllGroupMaterialsByUserId 17
 
 SELECT Tag.[Name] AS Tag, [Resource].Links, [Resource].Images, AllGroupMaterials.MaterialId
 FROM
---переиспользование GetAllGroupMaterialByUserId, как сделать? SELECT * FROM(EXEC имя_процедуры) не работает
-	(SELECT ResourceId, MaterialId, Links, Images FROM
-	(
-	SELECT Course_Material.MaterialId FROM Course_Material
-		WHERE [Course_Material].[CourseId] = (SELECT [Group].[CourseId] FROM [Group] WHERE Id=@GroupId)
-	UNION
-	SELECT Group_Material.MaterialId FROM Group_Material WHERE Group_Material.GroupId=@GroupId
-	) AS AllGroupMaterials
-	JOIN
-	[Material] ON AllGroupMaterials.MaterialId=Material.Id
-	JOIN
-	[Resource] On [Resource].Id=Material.ResourceId)
+
+(SELECT * FROM  @var)
 AS AllGroupMaterials
---конец переиспользования
+
 JOIN
 [Resource]ON [Resource].Id=AllGroupMaterials.ResourceId
 JOIN
