5afb98bdfbd06baa543beb1301c57610cc68c655
zeta0ne <space1signs@gmail.com>
2021-01-24 11:52:58 +0300

    displays course name and tags

--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectAllTagsOnCourse.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectAllTagsOnCourse.sql
@@ -1,7 +1,10 @@
 ﻿CREATE PROCEDURE [dbo].[SelectAllTagsOnCourse]
 as
-select * 
-	from Homework_Course as HC
-	inner join Homework_Tag as HT on HC.HomeworkId=HT.HomeworkId
-	inner join Course_Material as CM on HC.CourseId=CM.CourseId
-	right outer join Material_Tag as MT on HT.TagId=MT.TagId
+select distinct C.Name as CourseName, T.Name as TagName
+	from Course as C
+	inner join Homework_Course as HC on C.Id = HC.CourseId
+	inner join Homework_Tag as HT on HC.HomeworkId = HT.HomeworkId
+	inner join Course_Material as CM on HC.CourseId = CM.CourseId
+	inner join Material_Tag as MT on HT.TagId = MT.TagId
+	inner join Tag as T on MT.TagId = T.Id
+	where C.Id = 5
