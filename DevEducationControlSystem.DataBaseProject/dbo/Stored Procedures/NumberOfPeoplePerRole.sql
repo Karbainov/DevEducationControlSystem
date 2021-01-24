d0d8145374ed1cd5a2c612d586769fb6667882ad
first <pkpbi01@mail.ru>
2021-01-24 23:32:33 +0300

    Work With Procedures
    Add SelectHomeworkWithStatusesByUserId, Modify SelectAllMaterialsByGroupId and NumberOfPeoplePerRole

--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/NumberOfPeoplePerRole.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/NumberOfPeoplePerRole.sql
@@ -1,5 +1,5 @@
 ﻿CREATE PROCEDURE [dbo].[NumberOfPeoplePerRole]
 AS
-select Role.Name, COUNT(User_Role.Id) From [Role]
+select Role.Name, COUNT(User_Role.Id) NumberOfPeople From [Role]
 full join [User_Role]  on [User_Role].RoleId = [Role].Id
 Group By Role.Name
