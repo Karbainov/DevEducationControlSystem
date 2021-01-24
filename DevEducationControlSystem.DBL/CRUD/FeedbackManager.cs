38f472012795eba57d8e4b5d04c47ab87c8d04e8
Dmitriy Filimonov <banodannon@gmail.com>
2021-01-24 15:08:31 +0300

    DTOs created, names in description
    WholeCourseFeedbackDTO and ThemeFromCourseFeedbackDTO in DevEducationControlSystem.DBL.DTO.WholeCourseFeedback namespace

---
+++ DevEducationControlSystem.DBL/CRUD/FeedbackManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevEducationControlSystem.DBL.DTO.WholeCourseFeedback;
+
+namespace DevEducationControlSystem.DBL.CRUD
+{
+    public class FeedbackManager
+    {
+        string _connectionString;
+
+        public FeedbackManager()
+        {
+            _connectionString = @"Data Source=80.78.240.16; Initial Catalog=DevEdControl.Test;User Id=devEd; Password=qqq!11";
+        }
+    }
+}
