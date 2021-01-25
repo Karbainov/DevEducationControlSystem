e2e233821def6917357155e00998c61e4ccdc91d
julia <iulechkasirotkina@gmail.com>
2021-01-25 23:27:55 +0300

    Fix DTO

--- DevEducationControlSystem.DBL/DTO/SelectUserInfoByLoginDTO.cs
+++ DevEducationControlSystem.DBL/DTO/SelectUserInfoByLoginDTO.cs
@@ -4,7 +4,38 @@ using System.Text;
 
 namespace DevEducationControlSystem.DBL.DTO
 {
-    class SelectUserInfoByLoginDTO
+   public class SelectUserInfoByLoginDTO
     {
+           public int Id { get; set; }
+           public string FirstName { get; set; }
+           public string LastName { get; set; }
+           public string BirthDate { get; set; }
+           public string Email { get; set; }
+           public string Phone { get; set; }
+           public string ProfileImage { get; set; }
+           public string RoleName { get; set; }
+           public string GroupName { get; set; }
+           public string CourseName { get; set; }
+           public string CityName { get; set; }
+         //  public List <RoleDTO> roles { get; set; }
+
+        public SelectUserInfoByLoginDTO(SelectUserInfoByLoginDTO dto)
+        {
+            Id = dto.Id;
+            FirstName = dto.FirstName;
+            LastName = dto.LastName;
+            BirthDate = dto.BirthDate;
+            Email = dto.Email;
+            Phone = dto.Phone;
+            ProfileImage = dto.ProfileImage;
+            RoleName = dto.RoleName;
+            GroupName = dto.GroupName;
+            CourseName = dto.CourseName;
+            CityName = dto.CityName;
+        }
+
+
+
+
     }
 }
