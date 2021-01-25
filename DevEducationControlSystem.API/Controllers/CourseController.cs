c8b0afbf7cd763f48f55e8afda10a85cd3ab0e13
El_makso <El_makso@mail.ru>
2021-01-25 14:27:01 +0300

    +Primer

---
+++ DevEducationControlSystem.API/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevEducationControlSystem.BLL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevEducationControlSystem.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        [HttpGet("{courseId}")]
+        public IActionResult GetCourseInfoAndFeedbacksById(int courseId)
+        {
+            CoursePlusFeedbacksMapper feedbacksMapper = new CoursePlusFeedbacksMapper();
+            return new OkObjectResult(feedbacksMapper.GetCourseInfoAndFeedbacksByCourseId(courseId));
+        }
+    }
+}
