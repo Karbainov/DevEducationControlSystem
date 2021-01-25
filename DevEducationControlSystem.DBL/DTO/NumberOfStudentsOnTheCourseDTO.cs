d07f8b179b57ed5e5d00523e5751f0f91e6db373
Даниил <evkakiy.olen@gmail.com>
2021-01-25 03:12:53 +0300

    NumberOfStudentsOnTheCourseDTO

---
+++ DevEducationControlSystem.DBL/DTO/NumberOfStudentsOnTheCourseDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.DBL.DTO
+{
+    public class NumberOfStudentsOnTheCourse
+    {
+        public int GroupId { get; set; }
+        public int CourseId { get; set; }
+        public int UserGroup { get; set; }
+
+
+        public NumberOfStudentsOnTheCourse(int groupId, int courseId, int userGroup)
+        {
+            GroupId = groupId;
+            CourseId = courseId;
+            UserGroup = userGroup;
+        }
+
+    }
+}
+
+
+
--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/NumberOfStudentsOnTheCourse.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/NumberOfStudentsOnTheCourse.sql
@@ -2,7 +2,7 @@
 	@CourseId int	
 AS
   SELECT COUNT (MG.UserId)   
-  FROM Course 
+  FROM [Course] 
   inner join [Group] on Course.Id = [Group].CourseId  
   inner join [User_Group] as MG on [MG].GroupId = [Group].Id
   where Course.Id = @CourseId
