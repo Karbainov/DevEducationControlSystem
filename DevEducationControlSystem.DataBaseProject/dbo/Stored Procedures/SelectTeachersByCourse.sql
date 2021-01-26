e20578a4d87bdf8551ba68ed93f5e1ddaae1263d
zeta0ne <space1signs@gmail.com>
2021-01-27 01:46:51 +0300

    displays courseId, course name, city id, city name, amount

--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectTeachersByCourse.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectTeachersByCourse.sql
@@ -1,12 +1,11 @@
 ﻿CREATE PROCEDURE [dbo].[SelectTeachersByCourse]
-	@CourseId int
 as
-select C.Id as CourseId, C.[Name] as [Name], G.CityId as CityId, CT.[Name] as CityName
+select C.Id as CourseId, C.[Name] as [Name], G.CityId as CityId, CT.[Name] as CityName, count (C.Id) as Amount
 	from Course as C
 	inner join [Group] as G on C.Id=G.CourseId
 	inner join City as CT on G.CityId=CT.Id
 	inner join User_Group as UG on G.Id=UG.GroupId
 	inner join User_Role as UR on UG.UserId=UR.UserId
-	where G.CourseId = @CourseId
-	and UR.RoleId = '1'
+	where UR.RoleId = '1'
+	group by C.Id, C.[Name], G.CityId, CT.[Name]
 	go
