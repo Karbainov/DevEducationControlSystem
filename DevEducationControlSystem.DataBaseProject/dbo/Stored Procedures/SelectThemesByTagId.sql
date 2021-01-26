caa83ba2c072db668807ca6c7f76c8899a5a7994
zeta0ne <space1signs@gmail.com>
2021-01-27 02:12:35 +0300

    Select by TagId

--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectThemesByTagId.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectThemesByTagId.sql
@@ -1,7 +1,7 @@
-﻿CREATE PROCEDURE [dbo].[SelectThemesByTag]
-	@TagId int
+﻿CREATE PROCEDURE [dbo].[SelectThemesByTagId]
+@TagId int
 as
-select * 
+select T.Id as Id, T.[Name] as [Name]
 	from Material_Tag as MT
 	inner join Theme_Material as TM on TM.MaterialId=MT.MaterialId
 	inner join Theme as T on T.Id=TM.ThemeId
