e4041310775348eaee06e628abe05f6ac89551dc
zeta0ne <space1signs@gmail.com>
2021-01-27 03:22:35 +0300

    added SelectCourseByTagName

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectCourseByTagName.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[SelectCourseByTagName]
+@TagName nvarchar(100)
+as
+select T.[Name] as TagName, C.[Name] as CourseName
+from Tag as T
+inner join Theme_Tag as TT on TT.TagId = T.Id
+inner join Course_Theme as CT on CT.ThemeId = TT.ThemeId
+inner join Course as C on C.Id = CT.CourseId
+where T.[Name] = @TagName
