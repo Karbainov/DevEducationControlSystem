dda5eff6992127eb88d06effc801e07e3047650f
El_makso <El_makso@mail.ru>
2021-01-27 12:22:40 +0300

    GroupInfo in DBL

---
+++ DevEducationControlSystem.DBL/DTO/GroupWithCityAndStatusAndCourseDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.DBL.DTO
+{
+    public class GroupWithCityAndStatusAndCourseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public int StatusId { get; set; }
+        public string Status { get; set; }
+
+        public int CityId { get; set; }
+        public string City { get; set; }
+
+        public int CourseId { get; set; }
+        public string Course { get; set; }
+    }
+}
