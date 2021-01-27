1fb84c51d9a21f71a28a2b6e45f8c297917c534f
Dmitriy Filimonov <banodannon@gmail.com>
2021-01-27 03:12:26 +0300

    CourseDTO and Manager.SelectById() created

---
+++ DevEducationControlSystem.DBL/CRUD/CourseManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevEducationControlSystem.DBL.DTO.Base;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DevEducationControlSystem.DBL.CRUD
+{
+    public class CourseManager
+    {
+        public CourseDTO SelectById(int id)
+        {
+            CourseDTO courseDTO = new CourseDTO();
+            string expression = "[Course_SelectById]";
+            var parameter = new { Id=id };
+            using (var connection = SqlServerConnection.GetConnection())
+            {
+                courseDTO = connection.QuerySingle<CourseDTO>(expression, parameter, commandType: CommandType.StoredProcedure);
+            }
+            return courseDTO;
+        }
+    }
+}
---
+++ DevEducationControlSystem.DBL/DTO/Base/CourseDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.DBL.DTO.Base
+{
+    public class CourseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DurationWeeks { get; set; }
+        public bool IsDeleted { get; set; }
+        public CourseDTO()
+        {
+
+        }
+        public CourseDTO(int id, string name, string description, int durationWeeks, bool isDeleted)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            DurationWeeks = durationWeeks;
+            IsDeleted = isDeleted;
+        }
+    }
+}
