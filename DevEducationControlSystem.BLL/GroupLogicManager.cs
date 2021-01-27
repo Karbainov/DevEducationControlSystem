69648fb7bc71ec5b466c13cdbcbadf2da1915742
El_makso <El_makso@mail.ru>
2021-01-27 12:22:14 +0300

    CreateBaseEmptyClasses

---
+++ DevEducationControlSystem.BLL/GroupLogicManager.cs
@@ -0,0 +1,20 @@
+﻿using DevEducationControlSystem.BLL.Models;
+using DevEducationControlSystem.BLL.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.BLL
+{
+    public class GroupLogicManager
+    {
+        public GroupInfoModel GetGroupInfoById(int groupId)
+        {
+            //public GroupWithCityAndStatusAndCourseDTO SelectGroupWithCityAndStatusAndCourseById(int groupId)
+
+
+            var mapper = new GroupDTOUsersDTOMaterialsDTOHomeworksDTOtoGroupInfoModelMapper();
+            return mapper.Map();
+        }
+    }
+}
---
+++ DevEducationControlSystem.BLL/Mappers/GroupDTOUsersDTOMaterialsDTOHomeworksDTOtoGroupInfoModelMapper.cs
@@ -0,0 +1,15 @@
+﻿using DevEducationControlSystem.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.BLL.Mappers
+{
+    public class GroupDTOUsersDTOMaterialsDTOHomeworksDTOtoGroupInfoModelMapper
+    {
+        public GroupInfoModel Map()
+        {
+            return null;
+        }
+    }
+}
