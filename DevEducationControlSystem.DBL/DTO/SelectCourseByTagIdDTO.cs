66ee93c05bcfa04aae66b89d6b8fdb1286d95270
zeta0ne <space1signs@gmail.com>
2021-01-28 23:26:48 +0300

    DTO

---
+++ DevEducationControlSystem.DBL/DTO/SelectCourseByTagIdDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.DBL.DTO
+{
+    public class SelectCourseByTagIdDTO
+    {
+        public string TagName { get; set; }
+        public string CourseName { get; set; }
+
+        public SelectCourseByTagIdDTO()
+        {
+
+        }
+
+        public SelectCourseByTagIdDTO(SelectCourseByTagIdDTO dto)
+        {
+            TagName = dto.TagName;
+            CourseName = dto.CourseName;
+        }
+    }
+}
