f51a6aa0e9f69103c1df418f7e7f1613108b73d6
zeta0ne <space1signs@gmail.com>
2021-01-28 23:17:31 +0300

    added dapper

---
+++ DevEducationControlSystem.DBL/CRUD/TagManager.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using DevEducationControlSystem.DBL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DevEducationControlSystem.DBL.CRUD
+{
+    public class TagManager
+    {
+        public SqlConnection GetConnection()
+        {
+            string connectionString = @"Data Source=80.78.240.16; Initial Catalog=DevEdControl.Test;User Id=devEd; Password=qqq!11";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+
+        public List<SelectAllTagsOnCourseDTO> SelectAllTagsOnCourse(int id)
+        {
+            var themesList = new List<SelectAllTagsOnCourseDTO>();
+            string expr = "[SelectAllTagsOnCourse]";
+            var value = new { Id = id };
+
+            using (var connection = GetConnection())
+            {
+                themesList = connection.Query<SelectAllTagsOnCourseDTO>(expr, value, commandType: CommandType.StoredProcedure).AsList();
+            }
+
+            return themesList;
+        }
+    }
+}
