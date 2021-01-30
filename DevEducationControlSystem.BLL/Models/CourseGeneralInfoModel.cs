ea3592a3391871a66e2d8b3d9362f7227db622b8
Dmitriy Filimonov <banodannon@gmail.com>
2021-01-30 19:00:18 +0300

    models are added (see description)
    CourseGeneralInfoModel, StudentPublicInfoModel

---
+++ DevEducationControlSystem.BLL/Models/CourseGeneralInfoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.BLL.Models
+{
+    public class CourseGeneralInfoModel
+    {
+        public int CourseId;
+        public string CourseName;
+        public DateTime CourseStartDate;
+        public int DurationInWeeks;
+        public int GroupId;
+        public string GroupName;
+        public int TeacherId;
+        public string TeacherFirstName;
+        public string TeacherLastName;
+        public int TutorId;
+        public string TutorFirstName;
+        public string TutorLastName;
+        public List<StudentPublicInfoModel> StudentPublicInfoModels;
+    }
+}
