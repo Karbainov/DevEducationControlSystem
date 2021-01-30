4549cd8a216558501a6e13ffebd7e2bf96911d08
zeta0ne <space1signs@gmail.com>
2021-01-30 05:09:35 +0300

    added empty StatisticController

---
+++ DevEducationControlSystem.API/Controllers/StatisticController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEducationControlSystem.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatisticController: Controller
+    {
+        [HttpGet]
+        public IActionResult GetNumberOfTeachersByCourseId(int courseId)
+        {
+            return Ok();
+        }
+    }
+}
