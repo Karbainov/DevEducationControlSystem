6dfba0fb0bec47caec6f5de44690070841a351a8
Alina <leesha_@mail.ru>
2021-02-01 00:40:04 +0300

    dap done

--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectStudentsStudyingAfterBase.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectStudentsStudyingAfterBase.sql
@@ -1,5 +1,8 @@
-﻿CREATE PROCEDURE [dbo].[SelectStudentsStudyingAfterBase]
-as  select
+﻿CREATE PROCEDURE SelectStudentsStudyingAfterBase
+AS
+BEGIN
+
+select
 City.name as Cityname,
 [Group].name as Groupname,
 count( [User].id) as Usercount
@@ -13,3 +16,4 @@ left join [User_Role] on [User].id = User_Role.UserId
 Where [User].Statusid = 5
 and User_Role.RoleId = 2
 Group By City.name, [Group].name
+END
