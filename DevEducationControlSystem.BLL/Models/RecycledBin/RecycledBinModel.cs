bebe29eb9e14cb9045f3717138525e800ff032d2
Denis Valeev <valeevden@gmail.com>
2021-01-31 03:38:09 +0300

    Models for Mapper

---
+++ DevEducationControlSystem.BLL/Models/RecycledBin/RecycledBinModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.BLL.Models.RecycledBin
+{
+    class RecycledBinModel
+    {
+        public List<HomeworkModel> Homeworks { get; set; }
+
+        public List<MaterialModel> Materials { get; set; }
+
+        public List<CourseModel> Courses { get; set; }
+    }
+}
