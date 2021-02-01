69be00a7e7411e004cd5e2ecc60f66fd1bedea9d
Olga Radeeva <olgaradeev5947@mail.ru>
2021-02-01 03:30:21 +0300

    add procedure and dbl on it

---
+++ DevEducationControlSystem.DBL/DTO/CountStudentsOnCourseByGroupsDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.DBL.DTO
+{
+    public class CountStudentsOnCourseByGroupsDTO
+    {
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public int CountOfStudents { get; set; }
+
+        public CountStudentsOnCourseByGroupsDTO(int groupId, string groupName, int countOfStudents)
+        {
+            GroupId = groupId;
+            GroupName = groupName;
+            CountOfStudents = countOfStudents;
+
+        }
+
+
+
+
+    }
+}
