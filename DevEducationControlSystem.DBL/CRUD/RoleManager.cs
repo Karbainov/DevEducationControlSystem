e312bba54aabc3f4fa8c68af0f39d8c06c3e8002
NickWhiteman <romanov_nikita@hotmail.com>
2021-02-01 23:13:10 +0300

    DLL done

---
+++ DevEducationControlSystem.DBL/CRUD/RoleManager.cs
@@ -0,0 +1,18 @@
+﻿using Dapper;
+using DevEducationControlSystem.DBL.DTO.Base;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace DevEducationControlSystem.DBL.CRUD
+{
+    public class RoleManager
+    {
+        public RoleDTO SelectById(int id)
+        {
+            var RoleDTO = SqlServerConnection.GetConnection().Query<RoleDTO>("[Role_SelectById]", id, commandType: CommandType.StoredProcedure).Single<RoleDTO>();
+            return RoleDTO;
+        }
+    }
+}
