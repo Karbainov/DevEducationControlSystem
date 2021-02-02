39d295c10f07f1828f8581721520b8006b2256f1
Oleg Lyamzin <o.s.like@yandex.ru>
2021-02-02 19:31:22 +0300

    Create API layer for Appoint Homework

---
+++ DevEducationControlSystem.API/Controllers/HomeworkController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevEducationControlSystem.API.InputModels;
+using DevEducationControlSystem.BLL;
+using DevEducationControlSystem.BLL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevEducationControlSystem.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeworkController : Controller
+    {
+        [HttpPost("Appoint")]
+        public IActionResult AppointHomeworkToGroup(HomeworkAppointmentInputModel inputModel)
+        {
+            var homeworkLogicManager = new HomeworkLogicManager();
+            homeworkLogicManager.AppointHomeworkToGroup(new HomeworkAppointmentModel()
+            {
+                HomeworkId = inputModel.HomeworkId,
+                GroupId = inputModel.GroupId,
+                StartDate = inputModel.StartDate,
+                DeadLine = inputModel.DeadLine
+            });
+            return Ok();
+        }
+    }
+}
---
+++ DevEducationControlSystem.BLL/HomeworkLogicManager.cs
@@ -0,0 +1,15 @@
+﻿using DevEducationControlSystem.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEducationControlSystem.BLL
+{
+    public class HomeworkLogicManager
+    {
+        public void AppointHomeworkToGroup(HomeworkAppointmentModel homeworkAppointmentModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
