2f60805d3a7fefd77e83c7692da421431eb86bd2
SimonShvets <goodweather.spb@gmail.com>
2021-02-02 21:07:10 +0300

    UserCourse procedures Add

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/AddAnswerOnCurrentHomework.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[AddAnswerOnCurrentHomework]
+	@UserId int,
+	@ResourceId int,
+	@HomeWorkId int,
+    @Message NVARCHAR(1000),
+	@Links NVARCHAR(1000),
+	@Images NVARCHAR(1000)
+AS
+INSERT [dbo].[Answer] (UserId, ResourceId, [HomeworkId], [StatusId],[Date], [Message])
+VALUES(@UserId, @ResourceId, @HomeWorkId,7, GETDATE(), @Message)
+INSERT [dbo].[Resource] (Links, Images)
+VALUES(@Links, @Images)
