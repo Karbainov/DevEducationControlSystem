324b0ec95993523d24c4ebfe4e7173806c84f289
Oleg Lyamzin <o.s.like@yandex.ru>
2021-02-03 14:30:58 +0300

    Fix @HomeWorkId to @HomeworkId

--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/CRUD/Answer/Answer_Add.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/CRUD/Answer/Answer_Add.sql
@@ -1,10 +1,10 @@
 ﻿CREATE PROCEDURE [dbo].[Answer_Add]
 	@UserId int,
 	@ResourceId int,
-	@HomeWorkId int,
+	@HomeworkId int,
     @Date Datetime,
     @Message NVARCHAR(1000),
 	@StatusId int
 AS
 INSERT [dbo].[Answer] (UserId,ResourceId, [HomeworkId], [Date], [Message], StatusId)
-VALUES(@UserId, @ResourceId, @HomeWorkId, @Date, @Message, @StatusId)
+VALUES(@UserId, @ResourceId, @HomeworkId, @Date, @Message, @StatusId)
