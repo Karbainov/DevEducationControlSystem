c17884328904b8d5e0dfca167a22359baf2a0105
NickWhiteman <romanov_nikita@hotmail.com>
2021-02-03 13:44:37 +0300

    Create CourseControler/NewCourseInputModel

--- DevEducationControlSystem.API/Controllers/CourseController.cs
+++ DevEducationControlSystem.API/Controllers/CourseController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevEducationControlSystem.API.InputModels;
 using DevEducationControlSystem.BLL;
+using DevEducationControlSystem.BLL.Models;
+using DevEducationControlSystem.DBL.CRUD;
+using DevEducationControlSystem.DBL.DTO.Base;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +16,31 @@ namespace DevEducationControlSystem.API.Controllers
     [ApiController]
     public class CourseController : ControllerBase
     {
-        
+        [HttpPost("CreateCourse")]
+        public IActionResult CreateCourse(NewCourseInputModel inputModel)
+        {
+            var courseManager = new CourseManager();
+            courseManager.Add(inputModel.Name, inputModel.Description, inputModel.DurationInWeeks);
+
+            return Ok(courseManager);
+        }
+
+        [HttpPut("UpdateCourse/{courseid}/{isDeleted}")]
+        public IActionResult UpdateCourse(NewCourseInputModel inputModel, int courseid, int isDeleted)
+        {
+            var courseManager = new CourseManager();
+            courseManager.Update(courseid, inputModel.Name, inputModel.Description, inputModel.DurationInWeeks, Convert.ToBoolean(isDeleted));
+
+            return Ok(courseManager);
+        }
+
+        [HttpPost("DeleteCourse/{courseid}")]
+        public IActionResult DeleteCourse(int courseid)
+        {
+            var courseManager = new CourseManager();
+            courseManager.Delete(courseid);
+
+            return Ok(courseManager);
+        }
     }
 }
