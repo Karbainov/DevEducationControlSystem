9c5da617b7773d11025cfcfbe849f782a4ea2723
Dmitriy Filimonov <banodannon@gmail.com>
2021-02-04 02:07:20 +0300

    SelectHomeWorksAndAnswersByUserId stored procedure is added

---
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectHomeWorksAndAnswersByUserId.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[SelectHomeWorksAndAnswersByUserId]
+@UserId INT
+AS
+BEGIN
+
+SELECT * FROM
+Answer JOIN Homework ON Answer.HomeworkId=Homework.Id
+WHERE
+UserId = @UserId
+
+END
