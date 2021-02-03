9756022f2e7bb4e94dabe1e90c138493c4216050
NickWhiteman <romanov_nikita@hotmail.com>
2021-02-03 22:08:32 +0300

    process

--- DevEducationControlSystem.API/Controllers/AuthenticationController.cs
+++ DevEducationControlSystem.API/Controllers/AuthenticationController.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
 namespace DevEducationControlSystem.API.Controllers
@@ -13,28 +15,61 @@ namespace DevEducationControlSystem.API.Controllers
     [Route("[controller]")]
     public class AuthenticationController : Controller
     {
-        [HttpGet("{login}")]
-        public IActionResult Get(string login)
-        {
-            var logicManager = new AuthorizationLogicManager();
-            return Ok(logicManager.GetLoginPassworlRole(login));
-        }
-
-        //[HttpPut("{login}/{pass}")]
-        //public IActionResult Get(string login, string pass)
-        //{
-        //    var identity = GetIdentity(login, pass);
-        //    return Ok();
-        //}
-
-        //private ClaimsIdentity GetIdentity(string login, string pass)
+        //[HttpGet("{login}")]
+        //public IActionResult Get(string login)
         //{
         //    var logicManager = new AuthorizationLogicManager();
-        //    List<LoginPassRoleModel> list = logicManager.GetLoginPassworlRole();
-
-
-
-        //    return new ClaimsIdentity();
+        //    return Ok(logicManager.GetLoginPassworlRole(login));
         //}
+
+        [HttpGet("{login}/{pass}")]
+        public IActionResult Get(string login, string pass)
+        {
+            var identity = GetIdentity(login, pass);
+            if(identity == null)
+            {
+                return StatusCode(415,"Некорректно введен Логин или Пароль");
+            }
+
+            JwtSecurityToken token = new JwtSecurityToken
+            (
+                issuer: AuthenticationSettings.Issuer,
+                audience: AuthenticationSettings.Audience,
+                claims: identity.Claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.Add(TimeSpan.FromMinutes(AuthenticationSettings.Lifetime)),
+                signingCredentials: new SigningCredentials(AuthenticationSettings.SecurityKey, SecurityAlgorithms.HmacSha384)
+            );
+            string handler = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(handler);
+        }
+
+        private ClaimsIdentity GetIdentity(string login, string pass)
+        {
+            var logicManager = new AuthorizationLogicManager();
+            string role = "";
+            List<LoginPassRoleModel> list = logicManager.GetLoginPassworlRole(login);
+
+            foreach (var r in list)
+            {
+                if (login == r.UserLog && pass == r.UserPas)
+                {
+                    role = r.Roles[0].Name;
+                }
+            }
+            
+            if (role == "")
+            {
+                return null;
+            }
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, login),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, role)
+            };
+            
+            return new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+        }
     }
 }
