14aa8f69c63863b7ac0c386509c4455068dd04b2
Frendor <frendor.dm@gmail.com>
2021-02-04 18:49:34 +0300

    FicProcedureCommentByAnswer

--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectСommentByAnswerIdOrderByTime.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectСommentByAnswerIdOrderByTime.sql
@@ -1,6 +1,8 @@
-﻿CREATE PROCEDURE [dbo].[SelectСommentByAnswerIdOrderByTime]
+﻿create Proc [dbo].[SelectСommentByAnswerIdOrderByTime]
 @AnswerId int
 AS
-Select *
-From Comment
+Select c.Id, c.UserId,u.FirstName, u.LastName, c.AnswerId, c.ResourceId, c.Date, c.Message 
+From Comment c
+join [User] u on c.UserId = u.Id
 where AnswerId = @AnswerId
+Order by c.Date ASC
