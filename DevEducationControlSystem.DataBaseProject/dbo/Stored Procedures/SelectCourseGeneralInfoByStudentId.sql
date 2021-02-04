614463e3dcdfacceac9aa6ba00758d65c46c2d49
Dmitriy Filimonov <banodannon@gmail.com>
2021-02-05 01:39:17 +0300

    full join in CourseGeneralInfo. DeNull in Dapper

--- DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectCourseGeneralInfoByStudentId.sql
+++ DevEducationControlSystem.DataBaseProject/dbo/Stored Procedures/SelectCourseGeneralInfoByStudentId.sql
@@ -12,19 +12,19 @@ C.Id AS TeacherId, C.FirstName AS TeacherFirstName, C.LastName AS TeacherLastNam
 A.Id AS TutorId, A.FirstName AS TutorFirstName, A.LastName AS TutorLastName,
 B.Id AS StudentId, B.FirstName AS StudentFirstName, B.LastName AS StudentLastName
 FROM Course
-JOIN [Group] ON Course.Id=[Group].CourseId
-JOIN (SELECT * FROM User_Group WHERE User_Group.GroupId=@GroupId) AS D ON D.GroupId=[Group].Id
-JOIN 
+RIGHT JOIN [Group] ON Course.Id=[Group].CourseId
+RIGHT JOIN (SELECT * FROM User_Group WHERE User_Group.GroupId=1) AS D ON D.GroupId=[Group].Id
+FULL JOIN 
 (SELECT [User].Id, FirstName, LastName, GroupId FROM [User]
 	JOIN User_Role ON [User].Id=User_Role.UserId
 	JOIN User_Group ON [User].Id=User_Group.UserId
-	WHERE [User_Group].GroupId=@GroupId AND User_Role.RoleId=1) AS C ON C.Id=D.UserId
-JOIN
+	WHERE [User_Group].GroupId=1 AND User_Role.RoleId=1) AS C ON C.Id=D.UserId
+FULL JOIN
 (SELECT [User].Id, FirstName, LastName, GroupId FROM [User]
 	JOIN User_Role ON [User].Id=User_Role.UserId
 	JOIN User_Group ON [User].Id=User_Group.UserId
 	WHERE [User_Group].GroupId=@GroupId AND User_Role.RoleId=5) AS A ON C.GroupId=A.GroupId
-JOIN
+FULL JOIN
 (SELECT [User].Id, FirstName, LastName, GroupId FROM [User]
 	JOIN User_Role ON [User].Id=User_Role.UserId
 	JOIN User_Group ON [User].Id=User_Group.UserId
