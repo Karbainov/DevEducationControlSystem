b888a165bf66784bdb8c1b39d28e6cc7d2a6fda2
Dmitriy Filimonov <banodannon@gmail.com>
2021-02-05 03:48:52 +0300

    Id reviesed

--- DevEducationControlSystem.API/Controllers/GroupController.cs
+++ DevEducationControlSystem.API/Controllers/GroupController.cs
@@ -49,18 +49,30 @@ namespace DevEducationControlSystem.API.Controllers
         [Authorize(Roles = "Студент")]
         [HttpPut("Student/{userId}/private")]
         public IActionResult AddFeedback(List<NewFeedbackInputModel> feedback, int userId)
-        {
-            new GroupAPIManager().AddAndCheckNewFeedback(feedback, userId);
-
-            var groupLogicManager = new GroupLogicManager();
-            return Ok(groupLogicManager.GetPrivateStudentMainPageModel(userId));
+        {            if (IdRevieser.RevieseId(User.Identity.Name, userId) == true)
+            {
+                new GroupAPIManager().AddAndCheckNewFeedback(feedback, userId);
+                var groupLogicManager = new GroupLogicManager();
+                return Ok(groupLogicManager.GetPrivateStudentMainPageModel(userId));
+            }
+            else
+            {
+                return BadRequest("Доступ ограничен");
+            }
         }
         [Authorize(Roles = "Студент")]
         [HttpGet("User/{userId}/Materials/{tag}")]
         public IActionResult GetStudentUnlockedDataByTag(int userId, string tag)
         {
-            var groupLogicManager = new GroupLogicManager();
-            return Ok(groupLogicManager.GetStudentUnlockedMaterialsByTag(userId, tag));
+            if (IdRevieser.RevieseId(User.Identity.Name, userId) == true)
+            {
+                var groupLogicManager = new GroupLogicManager();
+                return Ok(groupLogicManager.GetStudentUnlockedMaterialsByTag(userId, tag));
+            }
+            else
+            {
+                return BadRequest("Доступ ограничен");
+            }
         }
 
         [Authorize(Roles = "Преподаватель")]
